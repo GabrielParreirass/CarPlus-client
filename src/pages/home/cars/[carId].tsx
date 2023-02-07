3fd6dfc1f43a73b71633cbba3c80261865cae7f8
Gabriel Parreiras <gparreiras46@gmail.com>
2023-02-06 22:09:12 -0300

    mudança regra de negócio

--- src/pages/home/cars/[carId].tsx
+++ src/pages/home/cars/[carId].tsx
@@ -36,30 +36,21 @@ export async function getStaticProps(context: any) {
   };
 }
 
-const carPage = ({
-  vehicle,
-}: {
-  vehicle: {
-    id: String;
-    brand: String;
-    model: String;
-    year: String;
-    price: String;
-    description: String;
-    class: String;
-  };
-}) => {
+const carPage = (vehicle: any) => {
   console.log(vehicle);
 
-  if (!vehicle) {
+  if (!vehicle.vehicle) {
     return <div>Carregando Infos</div>;
   }
 
-  const { combine, allowedMaxDays, beforeToday } = DateRangePicker;
+  const { combine, allowedMaxDays, beforeToday, after, before } =
+    DateRangePicker;
 
   const [inicialDate, setInicialDate] = useState(new Date());
   const [finalDate, setFinalDate] = useState(new Date());
   const [modal, setModal] = useState("hidden");
+  const [diarias, setDiarias] = useState(Number);
+  const [VehicleClass, SetVehicleClass] = useState("");
 
   const handleSubmit = () => {
     // console.log(`Início:${inicialDate.getDate()}/${inicialDate.getMonth()+1}`)
@@ -75,13 +66,30 @@ const carPage = ({
     let date1: any = new Date(d1);
     let date2: any = new Date(d2);
 
-    const diffInMs = date2 - date1;
-    const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
+    if (date2 > date1) {
+      const date1Formated = new Date(d1).toLocaleDateString("en-GB");
+      const date2Formated = new Date(d2).toLocaleDateString("en-GB");
 
-    console.log(diffInDays);
+      console.log({ date1Formated, date2Formated });
 
-    setModal("flex");
+      const diffInMs = date2 - date1;
+      const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
+
+      console.log(diffInDays);
+
+      setDiarias(diffInDays);
+
+      setModal("flex");
+    } else {
+      window.alert("Data inválida!")
+    }
   };
+
+  const handleCloseModal = () => {
+    setModal("hidden");
+  };
+
+  const modalVehicle = vehicle.vehicle[0];
 
   return (
     <>
@@ -95,27 +103,35 @@ const carPage = ({
       </div>
 
       <div className=" m-5 py-4 px-8 bg-indigo-500  flex flex-col justify-center items-center rounded">
-        <div className="text-white text-xl">
-          {vehicle.brand} - {vehicle.model}
+        <div className="text-white text-2xl">
+          Veiculos classe {modalVehicle.class}
         </div>
-        <div className="text-white text-lg">Ano {vehicle.year}</div>
-        <div className="text-white text-lg">Diária: R${vehicle.price},00</div>
-        <div className="text-sm">Classe {vehicle.class}</div>
-        <div>
-          {" "}
-          <Image
-            src={`/${vehicle.model}.webp`}
-            width="240"
-            height="240"
-            alt="hb20"
-          ></Image>{" "}
+
+        <div className="text-white text-lg mt-3">
+          Diária: R${modalVehicle.price},00
         </div>
+
+        <div className="flex">
+          {vehicle.vehicle.map((item: any) => (
+            <div>
+              {" "}
+              <Image
+                src={`/${item.model}.webp`}
+                width="240"
+                height="240"
+                alt="hb20"
+              ></Image>{" "}
+            </div>
+          ))}
+        </div>
+
         <div>
           <div className="text-xl font-bold text-white text-center p-3">
             Agendar aluguel
           </div>
           <div>
             <DateRangePicker
+              character="/"
               showOneCalendar
               placeholder="Selecione o tempo de aluguel"
               disabledDate={combine!(allowedMaxDays!(100), beforeToday!())}
@@ -131,21 +147,43 @@ const carPage = ({
           </div>
           <button
             onClick={() => handleSubmit()}
-            className="py-2 px-4 bg-slate-400 mt-4 rounded"
+            className="py-2 px-4 bg-cyan-300 mt-4 rounded"
           >
             Confirmar reserva
           </button>
         </div>
       </div>
 
-      <div>
+      <div className={modal}>
         <div className="bg-indigo-800 absolute left-1/2 right-1/2 w-1/2 h-96 -translate-x-1/2 -translate-y-1/2 text-white py-4 px-2 z-50 shadow-xl rounded-sm ">
-
           <div className="text-center font-semibold text-2xl">
             Deseja confirmar sua reserva?
           </div>
+          <div className="mt-5">
+            <div className="text-center text-lg">
+              {diarias} dias x R${modalVehicle.price},00
+            </div>
+            <div className="text-center text-lg">
+              Valor total = R${diarias * parseInt(modalVehicle.price)},00
+            </div>
+            <div className="text-center p-5">
+              <button className="py-2 px-4 bg-cyan-300 mt-4 rounded font-bold text-black">
+                Confirmar Reserva
+              </button>
+            </div>
+            <p className="m-auto max-w-md text-gray-400 mt-5">
+              Caso não existam veículos da classe selecionada no dia da
+              retirada, será provido um veiculo de classe igual ou superior para
+              o cliente.
+            </p>
+          </div>
 
-          <div className="text-black absolute right-3 top-3 bg-white p-2 rounded-full cursor-pointer">
+          <div
+            className="text-black absolute right-3 top-3 bg-white p-2 rounded-full cursor-pointer"
+            onClick={() => {
+              handleCloseModal();
+            }}
+          >
             X
           </div>
         </div>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { signOut, getSession } from "next-auth/react";
 import { GetServerSideProps, GetStaticProps } from "next/types";
 import { prisma } from "@/lib/prisma";
 import Image from "next/image";
 import Link from "next/link";
-import { useSession } from "next-auth/react"
+import { useSession } from "next-auth/react";
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const session = await getSession({ req });
 
   const vehicles = await prisma.vehicles.findMany();
 
-  console.log({session: session})
+  console.log({ session: session });
 
   return {
     props: { user: session!.user!, vehiclesData: vehicles },
@@ -19,10 +19,13 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
 };
 
 const index = (props: any) => {
-  console.log(props.user);
 
-  const data = useSession()
-  console.log(data)
+  const data = useSession();
+
+  const [vehiclesC, setVehiclesC] = useState(props.vehiclesData.filter((i:any)=> i.class == "C"))
+
+  const [vehiclesB, setVehiclesB] = useState(props.vehiclesData.filter((i:any)=> i.class == "B"))
+
 
   return (
     <div>
@@ -43,32 +46,88 @@ const index = (props: any) => {
         <h2 className="text-2xl text-center py-4">
           A maior variedade, para você escolher!
         </h2>
-        <div className="grid grid-cols-3">
-          {props.vehiclesData.map(
-            (vehicle: {
-              id: String;
-              brand: String;
-              model: String;
-              year: String;
-              price: String;
-              description: String;
-              class: String;
-              
-            }) => (
-              <div className=" m-5 py-4 px-8 bg-indigo-500  flex flex-col justify-center items-center rounded" >
-                <div className="text-white text-xl">
-                  {vehicle.brand} - {vehicle.model} 
+
+        <div>
+          <div className="flex">
+            <h2 className="ml-5 text-2xl">Classe C</h2>
+            <Link href={`/home/cars/C`}>
+                    <div className="text-white bg-indigo-400 py-2 px-4 cursor-pointer ml-5 rounded-xl">Ver detalhes</div>
+            </Link>
+          </div>
+          <div className="grid grid-cols-3">
+            {vehiclesC.map(
+              (vehicle: {
+                id: String;
+                brand: String;
+                model: String;
+                year: String;
+                price: String;
+                description: String;
+                class: String;
+              }) => (
+                <div className=" m-5 py-4 px-8 bg-indigo-500  flex flex-col justify-center items-center rounded">
+                  <div className="text-white text-xl">
+                    {vehicle.brand} - {vehicle.model}
+                  </div>
+                  <div className="text-white text-lg">Ano {vehicle.year}</div>
+                  <div className="text-white text-lg">
+                    Diária: R${vehicle.price},00
+                  </div>
+                  <div className="text-sm">Classe {vehicle.class}</div>
+                  <div>
+                    {" "}
+                    <Image
+                      src={`/${vehicle.model}.webp`}
+                      width="240"
+                      height="240"
+                      alt="hb20"
+                    ></Image>{" "}
+                  </div>
                 </div>
-                <div className="text-white text-lg">Ano {vehicle.year}</div>
-                <div className="text-white text-lg">Diária: R${vehicle.price},00</div>
-                <div className="text-sm">Classe {vehicle.class}</div>
-                <div> <Image src={`/${vehicle.model}.webp`} width="240" height="240" alt="hb20"></Image>  </div>
-                <Link href={`/home/cars/${vehicle.id}`}>
-                  <div className="text-white bg-indigo-400 py-2 px-4 cursor-pointer">Ver detalhes</div>
-                </Link>
-              </div>
-            )
-          )}
+              )
+            )}
+          </div>
+        </div>
+        <div>
+          <div className="flex">
+            <h2 className="ml-5 text-2xl">Classe B</h2>
+            <Link href={`/home/cars/C`}>
+                    <div className="text-white bg-indigo-400 py-2 px-4 cursor-pointer ml-5 rounded-xl">Ver detalhes</div>
+            </Link>
+          </div>
+          <div className="grid grid-cols-3">
+            {vehiclesB.map(
+              (vehicle: {
+                id: String;
+                brand: String;
+                model: String;
+                year: String;
+                price: String;
+                description: String;
+                class: String;
+              }) => (
+                <div className=" m-5 py-4 px-8 bg-indigo-500  flex flex-col justify-center items-center rounded">
+                  <div className="text-white text-xl">
+                    {vehicle.brand} - {vehicle.model}
+                  </div>
+                  <div className="text-white text-lg">Ano {vehicle.year}</div>
+                  <div className="text-white text-lg">
+                    Diária: R${vehicle.price},00
+                  </div>
+                  <div className="text-sm">Classe {vehicle.class}</div>
+                  <div>
+                    {" "}
+                    <Image
+                      src={`/${vehicle.model}.webp`}
+                      width="240"
+                      height="240"
+                      alt="hb20"
+                    ></Image>{" "}
+                  </div>
+                </div>
+              )
+            )}
+          </div>
         </div>
       </div>
     </div>
